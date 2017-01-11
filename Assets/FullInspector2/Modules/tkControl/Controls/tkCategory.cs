c77bde7e6b87236ade4df39732a1f9e8d5ca852a
gjroelofs <gj.roelofs@codepoke.net>
2017-01-11 13:18:20 +0000

    Add in Toolbar & Category

---
+++ Assets/FullInspector2/Modules/tkControl/Controls/tkCategory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace FullInspector
+{
+
+    public partial class tk<T, TContext>
+    {
+
+        /// <summary>
+        /// Will draw a GUI.Toolbar which enables a single Element as given by Add.
+        /// </summary>
+        public class Categories : tkControl<T, TContext>, IEnumerable
+        {
+
+            private class Category
+            {
+                public string name;
+                public tkControl<T, TContext> element;
+            }
+
+            private readonly List<Category> _categories;
+            private readonly VerticalGroup _holder;
+            private Toolbar _toolbar;
+
+            public Categories()
+            {
+                _toolbar = new Toolbar();
+                _holder = new VerticalGroup();
+                _categories = new List<Category>();
+
+                _holder.Add(_toolbar);
+            }
+
+            protected override T DoEdit(Rect rect, T obj, TContext context, fiGraphMetadata metadata)
+            {
+                return _holder.Edit(rect, obj, context, metadata);
+            }
+
+            protected override float DoGetHeight(T obj, TContext context, fiGraphMetadata metadata)
+            {
+                return _holder.GetHeight(obj, context, metadata);
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return _categories.GetEnumerator();
+            }
+
+            /// <summary>
+            /// Add the given element to this Categories Tool bar.
+            /// </summary>
+            public void Add(string name, tkControl<T, TContext> element)
+            {
+                int idx = _categories.Count;
+                _categories.Add(new Category { name = name, element = element });
+
+                _toolbar.Options = _categories.Select(category => category.name).ToArray();
+                _holder.Add(new ShowIf((input, context) => _toolbar.Selected == idx, element));
+            }
+
+        }
+    }
+
+}
---
+++ Assets/FullInspector2/Modules/tkControl/Controls/tkToolbar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+namespace FullInspector
+{
+
+    public partial class tk<T, TContext>
+    {
+
+        /// <summary>
+        /// Will draw a GUI.Toolbar inside the given rectangle.
+        /// </summary>
+        public class Toolbar : tkControl<T, TContext>
+        {
+
+            protected Action<T, TContext, int, string> _onClick;
+
+            public string[] Options { get; set; }
+            public int Selected { get; private set; }
+
+            public Toolbar(Action<T, TContext, int, string> onClick, params string[] options)
+            {
+                _onClick = onClick;
+                Options = options;
+            }
+
+            public Toolbar(Action<int, string> onClick, params string[] options) : this((t, cont, sel, name) => onClick(sel, name), options)
+            {
+            }
+
+            public Toolbar(params string[] options) : this((arg1, context, arg3, arg4) => { }, options)
+            {
+            }
+
+            protected override T DoEdit(Rect rect, T obj, TContext context, fiGraphMetadata metadata)
+            {
+                var newSelected = GUI.Toolbar(rect, Selected, Options);
+                if (newSelected == Selected) return obj;
+
+                Selected = newSelected;
+
+                if (_onClick != null){
+                    _onClick(obj, context, Selected, Options[Selected]);
+                }
+                return obj;
+            }
+
+            protected override float DoGetHeight(T obj, TContext context, fiGraphMetadata metadata)
+            {
+                return 18;
+            }
+        }
+    }
+
+}
